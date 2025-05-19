493a7f590a87406acdd9d282724fa7861bf4079b
Joao Devson Mucavel <joaodevsonmucavel@DevLabsM2.local>
2025-05-19 08:29:29 +0200

    Fix Globe Mash

--- src/components/Work/Work.jsx
+++ src/components/Work/Work.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import "./Work.css";
-import { Canvas, useLoader } from "@react-three/fiber";
+import { Canvas } from "@react-three/fiber"; //useLoader
 import { OrbitControls } from "@react-three/drei";
 import { TextureLoader } from 'three'
 
 const Work = () => {
-  const colorMap = useLoader(TextureLoader, '/src/assets/resources/PavingStones092_1K-JPG_Color.jpg')
+  // const colorMap = useLoader(TextureLoader, '/src/assets/resources/PavingStones092_1K-JPG_Color.jpg')
 
   return (
     <>
