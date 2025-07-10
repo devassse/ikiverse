14afd602f098b7b097a6e468aead26b32c6b7f19
devassse <joaodevsonm@gmail.com>
2025-07-10 11:42:46 +0200

    Landing page Complete

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,22 @@
+const openIcon = document.querySelector(".menu-icon");
+const closeIcon = document.querySelector(".close-menu-icon");
+const btn = document.getElementById("back-to-top");
 
-const openIcon = document.querySelector('.menu-icon');
-  const closeIcon = document.querySelector('.close-menu-icon');
+openIcon.addEventListener("click", function () {
+  openIcon.classList.add("hidden");
+  closeIcon.classList.remove("hidden");
+});
 
-  openIcon.addEventListener('click', function () {
-    openIcon.classList.add('hidden');
-    closeIcon.classList.remove('hidden');
-  });
+closeIcon.addEventListener("click", function () {
+  closeIcon.classList.add("hidden");
+  openIcon.classList.remove("hidden");
+});
 
-  closeIcon.addEventListener('click', function () {
-    closeIcon.classList.add('hidden');
-    openIcon.classList.remove('hidden');
-  });
+// back to top
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 800) {
+    btn.style.display = "block";
+  } else {
+    btn.style.display = "none";
+  }
+});
